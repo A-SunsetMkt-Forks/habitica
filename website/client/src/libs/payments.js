8d210485e6ef6802713f3c8887c972fee70d87a4
CuriousMagpie <eilatan@gmail.com>
2024-12-02 15:14:21 -0500

    fix(shared): fix to src/libs/payments
    commented out import and const store; change let to const on line 10

--- website/client/src/libs/payments.js
+++ website/client/src/libs/payments.js
@@ -1,13 +1,13 @@
-import getStore from '@/store';
+// import getStore from '@/store';
 
 export function setup () { // eslint-disable-line import/prefer-default-export
-  const store = getStore();
+  // const store = getStore();
 
   // Load the payment scripts
 
   // Stripe
   const stripeScript = document.createElement('script');
-  let firstScript = document.getElementsByTagName('script')[0];
+  const firstScript = document.getElementsByTagName('script')[0];
   stripeScript.async = true;
   stripeScript.src = 'https://js.stripe.com/v3/';
   firstScript.parentNode.insertBefore(stripeScript, firstScript);
